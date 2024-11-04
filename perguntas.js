a087d46b506ca16f25ae39d28a473788f6202937
Guilhe090 <167100972+Guilhe090@users.noreply.github.com>
2024-11-04 11:29:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'Em questão de decisões rapidas você seria rapido e eficiente?',
+    'Depende de Questões eu seria eficiente mais nao garanto eficiência.'
+)
+
+criaCartao(
+    'Marcas',
+    'Entre as marcas nike e adidas qual você escolheria para ser definitivo',
+    'Eu escolheria a nike pois e muito bonita'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual ano foi lançado Free fire',
+    '4 de dezembro de 2017'
+)
+
+criaCartao(
+    'Meio de transporte ',
+    'Porque o 99 e mais apropriado para meio de transporte? e porque?',
+    'Porque a eficacia e velocidade e melhor que transporte publico, exemplo: o 99 pode ser usado a qualquer momento ja o onibus tem horarios exatos'
+)
+
+criaCartao(
+    'supercarros',
+    'Entre lamborghini huracan e porshe 918 spyder qual seria a melhor escolha',
+    'A escolha e bem dificil mas escolheria a 918 spyder por eu curto mais e entendo sobre a porshe'
+)
